6e72aab3a9e4b747c244c0a8ae0e1a3018be5b61
Shawn Graham <shawn.graham@carleton.ca>
2025-02-11 13:25:51 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,11 +1,13 @@
-(function(){
-    // Create a TreeWalker to traverse all text nodes in the document body.
-    const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
+(function() {
+    const walker = document.createTreeWalker(
+        document.body,
+        NodeFilter.SHOW_TEXT,
+        null,
+        false
+    );
     let node;
     
-    // For each text node, remove all 't', 'q', and 'i' characters (both upper and lowercase).
     while (node = walker.nextNode()) {
-      node.nodeValue = node.nodeValue.replace(/[tqi]/gi, '');
+        node.nodeValue = node.nodeValue.replace(/woke/gi, 'basic human decency');
     }
-  })();
-  
+})();
